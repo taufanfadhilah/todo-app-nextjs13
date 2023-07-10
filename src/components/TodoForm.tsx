6271f38cf16b0f65179164ce7fd24403d62f28a0
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2023-07-10 15:01:19 +0700

    feat: setup and implement React Hook Form

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import React, { useRef } from "react";
+import React from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 // components
 import { Button } from "@/components/ui/button";
@@ -19,24 +22,29 @@ import { addTodo } from "@/store/todoSlice";
 // store
 import { store } from "@/store";
 
+type TodoForm = {
+  title: string;
+  note: string;
+};
+
+const schema = yup
+  .object({
+    title: yup.string().required(),
+    note: yup.string().required(),
+  })
+  .required();
+
 export default function TodoForm() {
-  const form = useRef<HTMLFormElement>(null);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm<TodoForm>({
+    resolver: yupResolver(schema),
+  });
 
-  const handleSubmit = (event: React.SyntheticEvent) => {
-    event.preventDefault();
-    const target = event.target as typeof event.target & {
-      title: {
-        value: string;
-      };
-      note: {
-        value: string;
-      };
-    };
-    const data = {
-      title: target.title.value,
-      note: target.note.value,
-    };
-
+  const onSubmit: SubmitHandler<TodoForm> = (data) => {
     store.dispatch(
       addTodo({
         title: data.title,
@@ -44,8 +52,7 @@ export default function TodoForm() {
         isDone: false,
       })
     );
-
-    form.current?.reset();
+    reset();
   };
 
   return (
@@ -56,21 +63,21 @@ export default function TodoForm() {
           Stay organized and boost your productivity with Task Tracker.{" "}
         </CardDescription>
       </CardHeader>
-      <form onSubmit={handleSubmit} ref={form}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <CardContent>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="title">Title</Label>
-              <Input id="title" name="title" />
+              <Input {...register("title")} error={errors.title?.message} />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="note">Note</Label>
-              <Input id="note" name="note" />
+              <Input {...register("note")} error={errors.note?.message} />
             </div>
           </div>
         </CardContent>
         <CardFooter className="flex justify-between">
-          <Button type="reset" variant="outline">
+          <Button variant="outline" onClick={() => reset()}>
             Reset
           </Button>
           <Button type="submit">Add</Button>
