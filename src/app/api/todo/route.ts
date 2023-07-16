a0248f7e477bbeb50a01284402ad0de82f15dd51
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2023-07-16 23:18:32 +0700

    feat: relation todo-user

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,29 +1,47 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 export async function GET() {
-  await prisma.$connect();
-  const todos = await prisma.todo.findMany({
-    orderBy: {
-      createdAt: "asc",
-    },
-  });
+  try {
+    const session = await getServerSession(authOptions);
 
-  return NextResponse.json({
-    success: true,
-    message: "Get all Todo",
-    data: todos,
-  });
+    const todos = await prisma.todo.findMany({
+      where: {
+        userId: session?.user.id,
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "Get all Todo",
+      data: todos,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Something went wrong",
+        data: error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
 
 export async function POST(req: NextRequest) {
-  await prisma.$connect();
+  const session = await getServerSession(authOptions);
   const data = await req.json();
 
   const todo = await prisma.todo.create({
     data: {
+      userId: session?.user.id,
       title: data.title,
       note: data.note,
     },
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -46,4 +46,18 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        session.user.id = token.uid;
+      }
+      return session;
+    },
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.uid = user.id;
+      }
+      return token;
+    },
+  },
 };
